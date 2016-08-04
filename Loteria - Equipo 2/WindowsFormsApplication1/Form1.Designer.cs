e80757220ac3cffd56437c0f75abbdfe97027c27
Andelous <andelous.aa@gmail.com>
2016-08-03 21:48:46 -0500

    Add files via upload

---
+++ Loteria - Equipo 2/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Loteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdGenerar1 = new System.Windows.Forms.Button();
+            this.p1f6 = new System.Windows.Forms.PictureBox();
+            this.p1f3 = new System.Windows.Forms.PictureBox();
+            this.p1f5 = new System.Windows.Forms.PictureBox();
+            this.p1f4 = new System.Windows.Forms.PictureBox();
+            this.p1f2 = new System.Windows.Forms.PictureBox();
+            this.p1f1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmdGenerar2 = new System.Windows.Forms.Button();
+            this.p2f6 = new System.Windows.Forms.PictureBox();
+            this.p2f3 = new System.Windows.Forms.PictureBox();
+            this.p2f5 = new System.Windows.Forms.PictureBox();
+            this.p2f4 = new System.Windows.Forms.PictureBox();
+            this.p2f2 = new System.Windows.Forms.PictureBox();
+            this.p2f1 = new System.Windows.Forms.PictureBox();
+            this.deck = new System.Windows.Forms.PictureBox();
+            this.ficha = new System.Windows.Forms.PictureBox();
+            this.cmdIniciar = new System.Windows.Forms.Button();
+            this.cmdSiguienteFicha = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFicha = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ficha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmdGenerar1);
+            this.groupBox1.Controls.Add(this.p1f6);
+            this.groupBox1.Controls.Add(this.p1f3);
+            this.groupBox1.Controls.Add(this.p1f5);
+            this.groupBox1.Controls.Add(this.p1f4);
+            this.groupBox1.Controls.Add(this.p1f2);
+            this.groupBox1.Controls.Add(this.p1f1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 375);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Papeleta 1";
+            // 
+            // cmdGenerar1
+            // 
+            this.cmdGenerar1.Location = new System.Drawing.Point(6, 331);
+            this.cmdGenerar1.Name = "cmdGenerar1";
+            this.cmdGenerar1.Size = new System.Drawing.Size(159, 38);
+            this.cmdGenerar1.TabIndex = 5;
+            this.cmdGenerar1.Text = "Generar";
+            this.cmdGenerar1.UseVisualStyleBackColor = true;
+            // 
+            // p1f6
+            // 
+            this.p1f6.Image = ((System.Drawing.Image)(resources.GetObject("p1f6.Image")));
+            this.p1f6.Location = new System.Drawing.Point(218, 175);
+            this.p1f6.Name = "p1f6";
+            this.p1f6.Size = new System.Drawing.Size(100, 150);
+            this.p1f6.TabIndex = 3;
+            this.p1f6.TabStop = false;
+            // 
+            // p1f3
+            // 
+            this.p1f3.Image = ((System.Drawing.Image)(resources.GetObject("p1f3.Image")));
+            this.p1f3.Location = new System.Drawing.Point(218, 19);
+            this.p1f3.Name = "p1f3";
+            this.p1f3.Size = new System.Drawing.Size(100, 150);
+            this.p1f3.TabIndex = 3;
+            this.p1f3.TabStop = false;
+            // 
+            // p1f5
+            // 
+            this.p1f5.Image = ((System.Drawing.Image)(resources.GetObject("p1f5.Image")));
+            this.p1f5.Location = new System.Drawing.Point(112, 175);
+            this.p1f5.Name = "p1f5";
+            this.p1f5.Size = new System.Drawing.Size(100, 150);
+            this.p1f5.TabIndex = 2;
+            this.p1f5.TabStop = false;
+            // 
+            // p1f4
+            // 
+            this.p1f4.Image = ((System.Drawing.Image)(resources.GetObject("p1f4.Image")));
+            this.p1f4.Location = new System.Drawing.Point(6, 175);
+            this.p1f4.Name = "p1f4";
+            this.p1f4.Size = new System.Drawing.Size(100, 150);
+            this.p1f4.TabIndex = 1;
+            this.p1f4.TabStop = false;
+            // 
+            // p1f2
+            // 
+            this.p1f2.Image = ((System.Drawing.Image)(resources.GetObject("p1f2.Image")));
+            this.p1f2.Location = new System.Drawing.Point(112, 19);
+            this.p1f2.Name = "p1f2";
+            this.p1f2.Size = new System.Drawing.Size(100, 150);
+            this.p1f2.TabIndex = 2;
+            this.p1f2.TabStop = false;
+            // 
+            // p1f1
+            // 
+            this.p1f1.Image = ((System.Drawing.Image)(resources.GetObject("p1f1.Image")));
+            this.p1f1.Location = new System.Drawing.Point(6, 19);
+            this.p1f1.Name = "p1f1";
+            this.p1f1.Size = new System.Drawing.Size(100, 150);
+            this.p1f1.TabIndex = 1;
+            this.p1f1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmdGenerar2);
+            this.groupBox2.Controls.Add(this.p2f6);
+            this.groupBox2.Controls.Add(this.p2f3);
+            this.groupBox2.Controls.Add(this.p2f5);
+            this.groupBox2.Controls.Add(this.p2f4);
+            this.groupBox2.Controls.Add(this.p2f2);
+            this.groupBox2.Controls.Add(this.p2f1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(349, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(325, 375);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Papeleta 2";
+            // 
+            // cmdGenerar2
+            // 
+            this.cmdGenerar2.Location = new System.Drawing.Point(6, 331);
+            this.cmdGenerar2.Name = "cmdGenerar2";
+            this.cmdGenerar2.Size = new System.Drawing.Size(159, 38);
+            this.cmdGenerar2.TabIndex = 4;
+            this.cmdGenerar2.Text = "Generar";
+            this.cmdGenerar2.UseVisualStyleBackColor = true;
+            // 
+            // p2f6
+            // 
+            this.p2f6.Image = ((System.Drawing.Image)(resources.GetObject("p2f6.Image")));
+            this.p2f6.Location = new System.Drawing.Point(218, 175);
+            this.p2f6.Name = "p2f6";
+            this.p2f6.Size = new System.Drawing.Size(100, 150);
+            this.p2f6.TabIndex = 3;
+            this.p2f6.TabStop = false;
+            // 
+            // p2f3
+            // 
+            this.p2f3.Image = ((System.Drawing.Image)(resources.GetObject("p2f3.Image")));
+            this.p2f3.Location = new System.Drawing.Point(218, 19);
+            this.p2f3.Name = "p2f3";
+            this.p2f3.Size = new System.Drawing.Size(100, 150);
+            this.p2f3.TabIndex = 3;
+            this.p2f3.TabStop = false;
+            // 
+            // p2f5
+            // 
+            this.p2f5.Image = ((System.Drawing.Image)(resources.GetObject("p2f5.Image")));
+            this.p2f5.Location = new System.Drawing.Point(112, 175);
+            this.p2f5.Name = "p2f5";
+            this.p2f5.Size = new System.Drawing.Size(100, 150);
+            this.p2f5.TabIndex = 2;
+            this.p2f5.TabStop = false;
+            // 
+            // p2f4
+            // 
+            this.p2f4.Image = ((System.Drawing.Image)(resources.GetObject("p2f4.Image")));
+            this.p2f4.Location = new System.Drawing.Point(6, 175);
+            this.p2f4.Name = "p2f4";
+            this.p2f4.Size = new System.Drawing.Size(100, 150);
+            this.p2f4.TabIndex = 1;
+            this.p2f4.TabStop = false;
+            // 
+            // p2f2
+            // 
+            this.p2f2.Image = ((System.Drawing.Image)(resources.GetObject("p2f2.Image")));
+            this.p2f2.Location = new System.Drawing.Point(112, 19);
+            this.p2f2.Name = "p2f2";
+            this.p2f2.Size = new System.Drawing.Size(100, 150);
+            this.p2f2.TabIndex = 2;
+            this.p2f2.TabStop = false;
+            // 
+            // p2f1
+            // 
+            this.p2f1.Image = ((System.Drawing.Image)(resources.GetObject("p2f1.Image")));
+            this.p2f1.Location = new System.Drawing.Point(6, 19);
+            this.p2f1.Name = "p2f1";
+            this.p2f1.Size = new System.Drawing.Size(100, 150);
+            this.p2f1.TabIndex = 1;
+            this.p2f1.TabStop = false;
+            // 
+            // deck
+            // 
+            this.deck.Image = global::WindowsFormsApplication1.Properties.Resources.graficable;
+            this.deck.Location = new System.Drawing.Point(12, 394);
+            this.deck.Name = "deck";
+            this.deck.Size = new System.Drawing.Size(100, 150);
+            this.deck.TabIndex = 5;
+            this.deck.TabStop = false;
+            // 
+            // ficha
+            // 
+            this.ficha.Image = global::WindowsFormsApplication1.Properties.Resources.graficable;
+            this.ficha.Location = new System.Drawing.Point(574, 394);
+            this.ficha.Name = "ficha";
+            this.ficha.Size = new System.Drawing.Size(100, 150);
+            this.ficha.TabIndex = 6;
+            this.ficha.TabStop = false;
+            // 
+            // cmdIniciar
+            // 
+            this.cmdIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.cmdIniciar.Location = new System.Drawing.Point(118, 414);
+            this.cmdIniciar.Name = "cmdIniciar";
+            this.cmdIniciar.Size = new System.Drawing.Size(212, 50);
+            this.cmdIniciar.TabIndex = 7;
+            this.cmdIniciar.Text = "Iniciar";
+            this.cmdIniciar.UseVisualStyleBackColor = true;
+            // 
+            // cmdSiguienteFicha
+            // 
+            this.cmdSiguienteFicha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.cmdSiguienteFicha.Location = new System.Drawing.Point(118, 470);
+            this.cmdSiguienteFicha.Name = "cmdSiguienteFicha";
+            this.cmdSiguienteFicha.Size = new System.Drawing.Size(212, 50);
+            this.cmdSiguienteFicha.TabIndex = 8;
+            this.cmdSiguienteFicha.Text = "Siguiente";
+            this.cmdSiguienteFicha.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 444);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Última ficha";
+            // 
+            // lblFicha
+            // 
+            this.lblFicha.AutoSize = true;
+            this.lblFicha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFicha.Location = new System.Drawing.Point(411, 470);
+            this.lblFicha.Name = "lblFicha";
+            this.lblFicha.Size = new System.Drawing.Size(85, 24);
+            this.lblFicha.TabIndex = 10;
+            this.lblFicha.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 556);
+            this.Controls.Add(this.lblFicha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdSiguienteFicha);
+            this.Controls.Add(this.cmdIniciar);
+            this.Controls.Add(this.ficha);
+            this.Controls.Add(this.deck);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lotería";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p1f6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1f1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p2f6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2f1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ficha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox p1f1;
+        private System.Windows.Forms.PictureBox p1f6;
+        private System.Windows.Forms.PictureBox p1f3;
+        private System.Windows.Forms.PictureBox p1f5;
+        private System.Windows.Forms.PictureBox p1f4;
+        private System.Windows.Forms.PictureBox p1f2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox p2f6;
+        private System.Windows.Forms.PictureBox p2f3;
+        private System.Windows.Forms.PictureBox p2f5;
+        private System.Windows.Forms.PictureBox p2f4;
+        private System.Windows.Forms.PictureBox p2f2;
+        private System.Windows.Forms.PictureBox p2f1;
+        private System.Windows.Forms.Button cmdGenerar1;
+        private System.Windows.Forms.Button cmdGenerar2;
+        private System.Windows.Forms.PictureBox deck;
+        private System.Windows.Forms.PictureBox ficha;
+        private System.Windows.Forms.Button cmdIniciar;
+        private System.Windows.Forms.Button cmdSiguienteFicha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFicha;
+    }
+}
+
